c5adf80128cf257ca24e4131c6bec54333852285
ahmadkeyhan <keyhan.ahmad@gmail.com>
2021-10-16 12:46:34 +0330

    Modified type component to use window object to retrieve the screen width and response to it

--- components/Type.js
+++ components/Type.js
@@ -5,6 +5,11 @@ import * as SiIcons from 'react-icons/si'
 import {motion} from 'framer-motion'
 
 export default function Type({id}) {
+    const [width, setWidth] = useState(0)
+    useEffect(() => {
+        setWidth(window.screen.width)
+    })
+    
     var type = null
     for (var i = 0; i < TypesData.length; i++) {
         if (id == TypesData[i].id) {
@@ -26,7 +31,7 @@ export default function Type({id}) {
 
     return (
         <div className={styles.Type} style={{backgroundColor: type.bgColor}}>
-            <div style={scrollY < 0 ? {display:'none'} : { opacity: scrollY }}
+            <div style={scrollY < 0 && width < 500 ? {display:'none'} : width < 500 ? { opacity: scrollY } : null}
                 className={styles.Banner}>
                 <motion.h1
                     animate={{ marginLeft: 0 }}
@@ -50,31 +55,33 @@ export default function Type({id}) {
                     loop
                     preload='auto' />
             </div>
-            <div className={styles.ParaBox}>
-                <h3 style={{color: type.bgColor}} className={styles.Heading}>Creative Strengths</h3>
-                <p className={styles.Paragraph}>{type.creativeStrength}</p>
-            </div>
-            <div className={styles.ParaBox}>
-                <h3 style={{color: type.bgColor}} className={styles.Heading}>Untapped Potential</h3>
-                <p className={styles.Paragraph}>{type.untappedPotential}</p>
-            </div>
-            <div className={styles.ParaBox}>
-                <h3 style={{color: type.bgColor}} className={styles.Heading}>Ideal Collaborator</h3>
-                <p className={styles.Paragraph}>{type.collabrator}</p>
-            </div>
-            <div className={styles.ParaBox}>
-                {type.definition.map((paragraph, index) => {
-                    return <p key={index} className={styles.Paragraph}>{paragraph}</p>
-                })}
-            </div>
-            <div style={{color: type.bgColor}} className={styles.DownloadBox}>
-                <p><a href={`/${type.id}Download.zip`} download>Download</a> your Creative Type</p>
-                <p>Share your Creative Type using <b>#mycreativetype</b></p>
-                <a href={`https://twitter.com/intent/tweet?hashtags=nextjs%2Cmycreativetype&text=${type.tweetText} Take the quiz to discover your type.&via=keyhansa1&url=https%3A%2F%2Fkeyhansa.ir`}>
-                    <div className={styles.TwitterIcon} style={{backgroundColor: type.bgColor}}>
-                        <SiIcons.SiTwitter />
-                    </div>
-                </a>
+            <div className={styles.ParaFlex}>
+                <div className={styles.ParaBox}>
+                    <h3 style={{color: type.bgColor}} className={styles.Heading}>Creative Strengths</h3>
+                    <p className={styles.Paragraph}>{type.creativeStrength}</p>
+                </div>
+                <div className={styles.ParaBox}>
+                    <h3 style={{color: type.bgColor}} className={styles.Heading}>Untapped Potential</h3>
+                    <p className={styles.Paragraph}>{type.untappedPotential}</p>
+                </div>
+                <div className={styles.ParaBox}>
+                    <h3 style={{color: type.bgColor}} className={styles.Heading}>Ideal Collaborator</h3>
+                    <p className={styles.Paragraph}>{type.collabrator}</p>
+                </div>
+                <div className={styles.ParaBox}>
+                    {type.definition.map((paragraph, index) => {
+                        return <p key={index} className={styles.Paragraph}>{paragraph}</p>
+                    })}
+                </div>
+                <div style={{color: type.bgColor}} className={styles.DownloadBox}>
+                    <p><a href={`/${type.id}Download.zip`} download>Download</a> your Creative Type</p>
+                    <p>Share your Creative Type using <b>#mycreativetype</b></p>
+                    <a href={`https://twitter.com/intent/tweet?hashtags=nextjs%2Cmycreativetype&text=${type.tweetText} Take the quiz to discover your type.&via=keyhansa1&url=https%3A%2F%2Fkeyhansa.ir`}>
+                        <div className={styles.TwitterIcon} style={{backgroundColor: type.bgColor}}>
+                            <SiIcons.SiTwitter />
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
     )
